e53b6d299eaeb995f965087ef42f972406a24947
HSH <s1132234@pu.edu.tw>
2025-09-15 17:14:40 +0800

    第五次commit (水平垂直置中)

--- app/src/main/java/tw/edu/pu/s1132234/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1132234/lotto/MainActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tw.edu.pu.s1132234.lotto.ui.theme.LottoTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,28 +43,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-
 fun Play(modifier: Modifier = Modifier) {
+    var lucky by remember { mutableStateOf((1..100).random()) }
 
-    var lucky = (1..100).random()
-
-    Column(modifier = modifier) {
-
-        Text(
-
-            text = "樂透數字(1-100)為 $lucky",
-
-            modifier = modifier
-
-        )
-        Button(
-
-            onClick = { lucky = (1..100).random() }
-
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.align(Alignment.Center)
         ) {
-
-            Text("重新產生樂透碼")
-
+            Text(text = "樂透數字(1-100)為 $lucky")
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { lucky = (1..100).random() }) {
+                Text("重新產生樂透碼")
+            }
         }
     }
 }
