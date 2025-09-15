1e5a8af22f6b4173e244465f70890c74e468393c
HSH <s1132234@pu.edu.tw>
2025-09-15 17:15:27 +0800

    第六次commit (狀態管理)

--- app/src/main/java/tw/edu/pu/s1132234/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1132234/lotto/MainActivity.kt
@@ -23,6 +23,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import tw.edu.pu.s1132234.lotto.ui.theme.LottoTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
